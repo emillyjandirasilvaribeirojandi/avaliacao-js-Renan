a9c65cff84676226c2365b4cb128a5c59b907de8
Emily Jandira <emillyjandirasilvaribeirojandi@gmail.com>
2025-12-08 22:18:36 -0300

    avaliacao.js

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# avaliacao-js-Renan
-avaliacao-js-Renan
+professor nao entendi o a perguntas 3
+
+vc vai pedir 10 numeros
+desses 10, vc vai responder quantos são negativos
+
+ou seja, a pessoa digita 10 numeros quaisquer, e vc informa quantos destes são negativos, tendeu?
---
+++ javascript/avaliacao.js
@@ -0,0 +1,30 @@
+1// FUO para dividir cinco numero
+// prompt serve para batata suaheasuiehieassiuehsuia
+const num =10
+const num5 = 5
+//       10/5
+const resultado = num/num5
+
+console.log('Dividindo 5 pessoas:')
+console.log('Dividindo  '+num+' por ' +num5)
+console.log(resultado)
+console.log('Dividinto dois')
+console.log('Dividindo 10')
+alert(resultado)
+
+
+'ATIVIDADE DA PERGUNTA2'
+ const numeros30= (prompt("Digito dois numero:"))
+const numero18= prompt("Digito dois numero:")
+alert("Resultado de " + numeros30 + "-"+ numero18 + "=" + (numeros30- numero18))
+resultado =12
+
+
+'atividade da perguntas3'
+
+// vc acabou misturando algumas coisas, nesse caso, vc nao usa console.log, somente alert e prompt, a parte da 2 ta indo bem, mas nao esta 100%, tente rever o que fizemos em aula
+// vc pode baixar aquele repositório que te mostrei agor pouco, assim consegue rever algumas coisas, 
+
+
+/// o unico arquivo que nao presta nesse repositório é o index.js, o restante vc pode revisar com calma, eles seguem a mesma lógica dessa sua atividade 2, tendeu?
+// belezinha, vc ta indo bem, só precisa praticar um pouco mais, na proxima aula revisamos, depois vc tira de letra (=
